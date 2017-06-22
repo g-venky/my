59a9049ee72346fb5a4adebac2d8c0b7acbf8fbe
venky <venkykly@gmail.com>
2017-06-23 00:24:29 +0530

    first commit

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,22 @@
+class StudentsController < ApplicationController
+ 
+ def index
+ 	@std = Student.all
+ end
+
+ def new
+ end
+
+ def show
+ end
+
+ def create
+ end
+
+ def update
+ end
+
+ def destroy
+ end
+
+end
